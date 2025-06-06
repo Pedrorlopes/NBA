5a9e9ce2d11325fcb4e88e0d75cc514e5157a23e
Pedrorlopes <pedrorlopes20@gmail.com>
2025-06-06 13:00:56 -0300

    connection.py

---
+++ connection.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://nba:nba123@db:5432/nba_db")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+def create_db():
+    Base.metadata.create_all(bind=engine)
